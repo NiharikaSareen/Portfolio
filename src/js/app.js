be018a0f922eb907241d194a42fb5436c3da7c7f
Niharika Sareen <niharika.sareen@slg.agency>
2020-10-04 19:48:27 +0100

    fixed audio url

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,6 @@ import "./modules/project-page";
 
 window.onload = function(){
   var audioButton = document.getElementById("audioButton");
- 
   if(audioButton){
       audioButton.addEventListener("click",AudioListener);
   }
@@ -19,20 +18,25 @@ window.onload = function(){
 
 function AudioListener() {
   var audio = document.getElementById("audio");
-  var soundIndicator = document.getElementById('sound-container__indicator');
-  
+  var soundIndicator = document.getElementById('sound-container__indicator'); 
   if(audio.paused){
-    audio.play();
-    soundIndicator.innerHTML = "Audio On";
-    setTimeout(function(){ 
-      soundIndicator.innerHTML = "Audio";
-  }, 3000);
+    var audioPlay= audio.play();
+    if(audioPlay !==undefined){
+      audioPlay.then(function(){
+      soundIndicator.innerHTML = "Audio On";
+      setTimeout(function(){ 
+        soundIndicator.innerHTML = "Audio";
+      }, 3000);
+      }).catch(function(error){
+        console.log(error);
+      })
+    }  
   }
   else{
     audio.pause();
     soundIndicator.innerHTML = "Audio Off";
     setTimeout(function(){ 
       soundIndicator.innerHTML = "Audio";
-  }, 3000);
+    }, 3000);
   }
 }
